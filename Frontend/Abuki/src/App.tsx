3de540201a17a1f9721b9499bc98bc433335187c
Reyan <reyann.14b@gmail.com>
2025-02-13 19:44:14 +0300

    made the UI more functional and better, still not finished

--- Frontend/Abuki/src/App.tsx
+++ Frontend/Abuki/src/App.tsx
@@ -1,24 +1,25 @@
+import "./index.css"
 import { Outlet } from "react-router-dom";
-import Aside from "./components/Aside";
+import Aside from "./components/Aside/Aside";
 import Navbar from "./components/Navbar";
-import "./index.css";
+import img from "./assets/abuki.png";
+import { useState } from "react";
 
 const App = () => {
+  const[isCollapsed,setisCollapsed]=useState(false)
+  const changeCollapsed=()=>{
+    setisCollapsed(!isCollapsed)
+  }
   return (
-    <div className="contatiner">
-      <div className="row">
-        <Navbar />
-      </div>
-      <div className="row">
-        <div className="col">
-          <Aside />
-        </div>
-        <div className="col">
-          <Outlet />
-        </div>
+    <div>
+      <Navbar changeCollapsed={changeCollapsed}/>
+      <div className="grid-cont">
+        <Aside isCollapsed={isCollapsed}/>
+        <Outlet/>
       </div>
     </div>
   );
 };
 
 export default App;
+<Aside />
--- Frontend/Abuki/src/pages/Dashboard.tsx
+++ Frontend/Abuki/src/pages/Dashboard.tsx
@@ -1,17 +1,25 @@
-import Expense from "../components/Expense"
-import Sold from "../components/Sold"
-import Profit from "../components/Profit"
-import Report from "../components/Report"
+import Expense from "../components/Expense";
+import Sold from "../components/Sold";
+import Profit from "../components/Profit";
+import Report from "../components/Report";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div className="d-flex justify-content-around g-3">
-      <Sold Sold={100}/>
-      <Expense Expense={100}/>
-      <Profit Profit={100}/>
-      <Report generate={() => console.log("Generating report")}/>
-    </div>
-  )
-}
+    <>
+      <div className="dashboard-container">
+        <div className="metrics-container">
+          <Sold Sold={100} />
+          <Expense Expense={100} />
+          <Profit Profit={100} />
+        </div>
+        <div className="chart-container">
+          <div className="chart-placeholder"></div>
+        </div>
+        <Report generate={() => console.log("Generating report")} />
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
