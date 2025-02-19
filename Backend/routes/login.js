39a75a9ce1fabbf5afa792cff02e6c5ab603af95
Reyan <reyann.14b@gmail.com>
2025-02-19 17:52:13 +0300

    ...

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -23,33 +23,6 @@ if(!result.isEmpty()) return res.status(500).send("Error: ", result.array())
 
 res.sendStatus(200)
 })
-// router.post("/login", checkSchema(createLoginSchema), async (req, res) => {
-//   const { body } = req;
-//   const { password, username } = body;
-//   console.log(body);
-//   const result = validationResult(req);
-//   if (!result.isEmpty()) {
-//     return res.status(400).send({ errors: result.array() });
-//   }
-//   const verify=await Login.findOne({"username":username})
-//   if(!(verify)){return res.send("User not found").status(404)}
-//   const compare=await bcrypt.compare(password, verify?.password)
-//   if(compare){
-//     try {
-//       req.session.user = username;
-//       req.session.visited = true;
-//       res.status(200).send(body);
-//     } catch (err) {
-//       res.status(500).send(err.message);
-//       console.log(err);
-//     }
-//   }
-// else{
-//     console.log(compare, verify.password, password,)
-//     res.send("Invalid credentials").status(404)
-// }
-  
-// });
 
 
 export default router
